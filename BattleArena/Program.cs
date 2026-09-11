c209ce3466aa29fb5657c837d25ae09c7bb1ae03
Assherry1 <patrickdc9698@gmail.com>
2026-09-11 13:40:36 +0800

    Add battle loop and special attacks to Warrior class

--- BattleArena/Program.cs
+++ BattleArena/Program.cs
@@ -1,36 +1,31 @@
 ﻿using BattleArena.Warriors;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BattleArena
 {
-
     internal class Program
     {
         static void Main(string[] args)
         {
-            Warrior Raymond = new Warrior("Raymond", 100, 30);
-            Warrior Kirk = new Warrior("Kirk", 200, 15);
-            Warrior Crisy = new Warrior("Crisy", 150, 25);
+            int round = 1;
+            Warrior Raymond = new Warrior("Raymond", 100, 30, "Dinuraan");
+            Warrior Kirk = new Warrior("Kirk", 200, 15, "Dinaganan");
+            Warrior Crisy = new Warrior("Crisy", 150, 30, "Fire Ball");
 
+            Raymond.DisplayStats();
+            Kirk.DisplayStats();
+            Crisy.DisplayStats();
 
+            while (Raymond.IsAlive && Kirk.IsAlive && Crisy.IsAlive)
+            {
+                Console.WriteLine($"------Round {round}------");
+                Raymond.Attack(Kirk);
+                Kirk.Attack(Crisy);
+                Crisy.Attack(Raymond);
+                Console.WriteLine("----------------");
+                round++;
+            }
 
-
-            Console.WriteLine($"{Raymond.Name} has " +
-                $"{Raymond.Health} health and {Raymond.AttackPower} attack power.");
-            Console.WriteLine("---------------------------------");
-            Console.WriteLine($"{Kirk.Name} has" +
-                $" {Kirk.Health} health and {Kirk.AttackPower} attack power.");
-            Console.WriteLine("---------------------------------");
-            Console.WriteLine($"{Crisy.Name} has" +
-                $" {Crisy.Health} health and {Crisy.AttackPower} attack power.");
-            Console.WriteLine("---------------------------------");
-            Console.WriteLine($"{Crisy.Name} has" +
-                $" {Crisy.Health} health and {Crisy.AttackPower} attack power.");
-            Console.WriteLine("---------------------------------");
             Console.ReadKey();
         }
     }
